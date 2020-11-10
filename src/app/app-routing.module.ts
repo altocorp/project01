327b109784e27a3770406f07d0e0254fc94fe88c
Alto Corp <altosmithcorp@gmail.com>
2020-11-10 13:27:25 +0100

    -

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomepageComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
